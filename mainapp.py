6aebdce6aa7a2216991e42a7da0b9168a249165f
jibin koshy <jibinkoshy07@gmail.com>
2023-02-20 18:40:49 +0530

    2023-02-20 18:40:49

---
+++ mainapp.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+import pymysql
+
+app = Flask (__name__)
+
+class Database:
+	def __init__(self):
+		host = "127.0.0.1"
+		user = "flask_user"
+		password= "jibu007"
+		db = "flask_app"
+		self.con = pymysql.connect(host=host, user=user, password=password, db=db,
+			     cursorclass=pymysql.cursors.DictCursor)
+		self.cur = self.con.cursor()
+
+	def list_products(self):
+		self.cur.execute("select pid, name, price from product")
+		result = self.cur.fetchall()
+		return result
+		
+
+
+@app.route('/home') 
+def home ():
+	return render_template("home.html")
+
+@app.route('/product')
+def product ():
+	def db_query():
+		db = Database()
+		products = db.list_products()
+		return products
+	res = db_query()
+		
+
+	return render_template("product.html", result=res)
